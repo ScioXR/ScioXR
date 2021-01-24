91aa0d88437286a6b8af5b470c5a65b551005279
Adrian Djura <adrian.djura@gmail.com>
2021-01-24 09:50:58 +0100

    joystick move improvements

--- Assets/Scripts/Platforms/WebXR/WebXRMoveController.cs
+++ Assets/Scripts/Platforms/WebXR/WebXRMoveController.cs
@@ -9,9 +9,29 @@ public class WebXRMoveController : MonoBehaviour
 {
     public float walkingSpeed = 1.0f;
 
+    public enum ForwardMove
+    {
+        NONE,
+        WALK,
+    }
+
+    public enum HorizontalMove
+    {
+        NONE,
+        STRAFE,
+        ROTATE
+    }
+
+    public ForwardMove forwardMoveType;
+    public HorizontalMove horizontalMoveType;
+
+    public float rotateSpeed;
+
     WebXRControllerDevice controllerDevice;
 
     public WebXRControllerHand hand;
+
+    private bool waitRelease;
 
     // Start is called before the first frame update
     void Start()
@@ -24,21 +44,40 @@ public class WebXRMoveController : MonoBehaviour
     {
         if (CameraHelper.main)
         {
-            Vector3 forward = CameraHelper.main.transform.TransformDirection(Vector3.forward);
-            forward.y = 0;
-            Vector3 right = CameraHelper.main.transform.TransformDirection(Vector3.right);
-            right.y = 0;
-
             float thumbstickX = controllerDevice.thumbstick.x.ReadValue();
             float thumbstickY = controllerDevice.thumbstick.y.ReadValue();
 
-            if (thumbstickY != 0)
+            if (forwardMoveType == ForwardMove.WALK)
             {
-                transform.position += forward * walkingSpeed * thumbstickY * Time.deltaTime;
+                Vector3 forward = CameraHelper.main.transform.TransformDirection(Vector3.forward);
+                forward.y = 0;
+                Vector3 right = CameraHelper.main.transform.TransformDirection(Vector3.right);
+                right.y = 0;
+
+                if (thumbstickY != 0)
+                {
+                    transform.position += forward * walkingSpeed * thumbstickY * Time.deltaTime;
+                }
+                if (thumbstickX != 0)
+                {
+                    if (horizontalMoveType == HorizontalMove.STRAFE)
+                    {
+                        transform.position += right * walkingSpeed * thumbstickX * Time.deltaTime;
+                    }
+                }
             }
-            if (thumbstickX != 0)
+
+            if (horizontalMoveType == HorizontalMove.ROTATE)
             {
-                transform.position += right * walkingSpeed * thumbstickX * Time.deltaTime;
+                bool pressed = controllerDevice.thumbstick.x.IsPressed(0.5f);
+                if (!waitRelease && pressed)
+                {
+                    transform.Rotate(Vector3.up, rotateSpeed * (thumbstickX > 0 ? 1 : -1));
+                    waitRelease = true;
+                } else if (waitRelease && !pressed)
+                {
+                    waitRelease = false;
+                }
             }
         }
     }
